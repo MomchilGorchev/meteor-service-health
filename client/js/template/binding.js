2d6fbe1215d78648040e1d27461b3ac21f9e6b3e
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-10-14 17:33:27 +0100

    Switching to simpler layout to display all endpoint in one list

--- client/js/template/binding.js
+++ client/js/template/binding.js
@@ -2,85 +2,10 @@
  * Created by momchillgorchev on 22/05/15.
  */
 
-Template.home.helpers({
-
-    epAtlassian: function(){
-        return Endpoints.find({category: 'Atlassian'});
-    },
-    epAtlassianCount: function(){
-        return Endpoints.find({category: 'Atlassian'}).count();
-    },
-
-    epPowaTAG: function(){
-        return Endpoints.find({category: 'PowaTAG'});
-    },
-    epPowaTAGCount: function(){
-        return Endpoints.find({category: 'PowaTAG'}).count();
-    },
-
-    epPowaWEB: function(){
-        return Endpoints.find({category: 'PowaWEB'});
-    },
-    epPowaWEBCount: function(){
-        return Endpoints.find({category: 'PowaWEB'}).count();
-    },
-
-    epPowaAIM: function(){
-        return Endpoints.find({category: 'PowaAIM'});
-    },
-    epPowaAIMCount: function(){
-        return Endpoints.find({category: 'PowaAIM'}).count();
-    },
-
-    epHK: function(){
-        return Endpoints.find({category: 'HK'});
-    },
-    epHKCount: function(){
-        return Endpoints.find({category: 'HK'}).count();
-    }
+Template.registerHelper('endpoints', function(){
+    return Endpoints.find();
 });
 
-Template.editServicesGrid.helpers({
-
-    epAtlassian: function(){
-        return Endpoints.find({category: 'Atlassian'});
-    },
-    epAtlassianCount: function(){
-        return Endpoints.find({category: 'Atlassian'}).count();
-    },
-
-    epPowaTAG: function(){
-        return Endpoints.find({category: 'PowaTAG'});
-    },
-    epPowaTAGCount: function(){
-        return Endpoints.find({category: 'PowaTAG'}).count();
-    },
-
-    epPowaWEB: function(){
-        return Endpoints.find({category: 'PowaWEB'});
-    },
-    epPowaWEBCount: function(){
-        return Endpoints.find({category: 'PowaWEB'}).count();
-    },
-
-    epPowaAIM: function(){
-        return Endpoints.find({category: 'PowaAIM'});
-    },
-    epPowaAIMCount: function(){
-        return Endpoints.find({category: 'PowaAIM'}).count();
-    },
-
-    epHK: function(){
-        return Endpoints.find({category: 'HK'});
-    },
-    epHKCount: function(){
-        return Endpoints.find({category: 'HK'}).count();
-    }
-});
-
-Template.footer.helpers({
-    powaInfo: function(){
-        var powa = new Powa('Powa Frontend');
-        return powa || 'Collection empty';
-    }
+Template.registerHelper('endpointsCount', function(){
+    return Endpoints.find().count();
 });
