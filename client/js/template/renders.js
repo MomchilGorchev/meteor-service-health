2f2928b414963ee0687e6c45c00e7860dc700f39
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-10-14 17:48:52 +0100

    Layout changes

--- client/js/template/renders.js
+++ client/js/template/renders.js
@@ -10,26 +10,26 @@ Template.header.rendered = function(){
     });
 };
 
-Template.home.rendered = function(){
-    var tabs = $('.tabs');
-    tabs.tabs();
-
-    setInterval(function(){
-        var tabsAmount = tabs.find('li').length,
-            currentTab = tabs.find('a.active'),
-            targetID = currentTab.attr('href'),
-            tabIndex = +targetID.slice(2);
-        if (tabIndex + 1 > tabsAmount){
-           tabIndex = 0;
-        }
-        var newTarget = '#t' + parseInt(tabIndex + 1);
-        tabs.find('a[href^="'+ newTarget +'"]').trigger('click');
-    }, 1000 * 30);
-
-    Meteor.call('checkServicesStatus', function(err, res){
-        err ? console.log(err) : console.log(res);
-    });
-};
+//Template.home.rendered = function(){
+//    var tabs = $('.tabs');
+//    tabs.tabs();
+//
+//    setInterval(function(){
+//        var tabsAmount = tabs.find('li').length,
+//            currentTab = tabs.find('a.active'),
+//            targetID = currentTab.attr('href'),
+//            tabIndex = +targetID.slice(2);
+//        if (tabIndex + 1 > tabsAmount){
+//           tabIndex = 0;
+//        }
+//        var newTarget = '#t' + parseInt(tabIndex + 1);
+//        tabs.find('a[href^="'+ newTarget +'"]').trigger('click');
+//    }, 1000 * 30);
+//
+//    Meteor.call('checkServicesStatus', function(err, res){
+//        err ? console.log(err) : console.log(res);
+//    });
+//};
 
 Template.addServiceEndpoint.rendered = function(){
     $('select').material_select();
