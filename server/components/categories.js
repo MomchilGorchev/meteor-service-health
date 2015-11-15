b0ee558b3e1dbe6f299851a1a9b07ca78c5afea7
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-11-15 14:18:59 +0000

    Temp removing velocity

--- server/components/categories.js
+++ server/components/categories.js
@@ -1,3 +1,28 @@
 /**
  * Created by momchillgorchev on 15/11/15.
+ * Categories related server methods
  */
+
+
+
+Meteor.startup(function() {
+
+    return Meteor.methods({
+
+        saveNewCategories: function(categories){
+
+            check(categories, Array);
+
+            for(var i = 0, count = categories.length; i < count; i++){
+
+                log(categories[i]);
+
+
+
+            }
+
+        }
+
+    });
+
+});
