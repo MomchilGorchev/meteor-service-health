87fcd4f8079081fd1b3c445e0d955ce8b4ec406e
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-11-18 23:03:51 +0000

    Quick info section

--- client/global/scripts/template-helpers.js
+++ client/global/scripts/template-helpers.js
@@ -6,12 +6,36 @@ Template.registerHelper('endpoints', function(){
     return Endpoints.find({}, {sort: {order: -1}});
 });
 
+/**
+ * Fetch all endpoints and check they last status
+ */
 Template.registerHelper('endpointsCount', function(){
-    return Endpoints.find().count();
-});
+    // Fetch the data once
+    var allData = Endpoints.find().fetch(),
+        buffer = {
+            green: 0,
+            red: 0,
+            orange: 0
+        };
 
-Template.registerHelper('endpointsGreenCount', function(){
-    return Endpoints.find({"lastStatusCode": 200}).count();
+    // Iterate over the data and check the status color code
+    for(var i = 0, count = allData.length; i < count; i++){
+        var current = allData[i];
+
+        if(current.status === 'green'){
+            buffer.green++;
+        } else if(current.status === 'orange'){
+            buffer.orange++;
+        } else {
+            buffer.red++;
+        }
+    }
+
+    // Calculate the total amount of processed entries
+    // if there is an entry with no field (bad data), it wont be considered
+    buffer.total = buffer.green + buffer.red + buffer.orange;
+    return buffer;
+
 });
 
 Template.registerHelper('equals', function (a, b) {
