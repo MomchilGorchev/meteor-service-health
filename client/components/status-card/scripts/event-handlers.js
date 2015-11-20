fc900e63221b89c6545c8bc5d7e348eb5b9e87ea
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-11-20 00:50:00 +0000

    Controls events started

--- client/components/status-card/scripts/event-handlers.js
+++ client/components/status-card/scripts/event-handlers.js
@@ -1,3 +1,24 @@
 /**
  * Created by momchillgorchev on 20/10/15.
  */
+
+
+Template.statusCard.events({
+
+    'click .card__reload': function(e, t){
+
+        e.preventDefault();
+
+        var trigger = $(e.currentTarget),
+            parent = trigger.closest('.card'),
+            cardId = parent.attr('data-itemid');
+
+        trigger.find('i').addClass('rotating');
+        log(cardId);
+
+        // TODO implement server functionality to
+        // refresh single service
+
+    }
+
+});
