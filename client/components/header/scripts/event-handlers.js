124a3280f6da78abd2a1ac9acd24b05770ab3e35
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-11-21 00:25:05 +0000

    Abstracting the seach bar into template and move stuff arround

--- client/components/header/scripts/event-handlers.js
+++ client/components/header/scripts/event-handlers.js
@@ -20,3 +20,18 @@ Template.header.events({
         });
     }
 });
+
+Template.titleAndSearch.events({
+    'keyup #search': function(e, t){
+        var filter = $(e.currentTarget).val();
+        var items = $('.card');
+        $.each(items, function(){
+            var current = $(this);
+            if(current.text().search(new RegExp(filter, 'i')) < 0){
+                current.hide();
+            } else {
+                current.show();
+            }
+        });
+    }
+});
--- client/global/scripts/event-handlers.js
+++ client/global/scripts/event-handlers.js
@@ -1,18 +1,3 @@
 /**
  * Created by momchillgorchev on 20/10/15.
  */
-
-Template.home.events({
-    'keyup #search': function(e, t){
-        var filter = $(e.currentTarget).val();
-        var items = $('.card');
-        $.each(items, function(){
-            var current = $(this);
-            if(current.text().search(new RegExp(filter, 'i')) < 0){
-                current.hide();
-            } else {
-                current.show();
-            }
-        });
-    }
-});
