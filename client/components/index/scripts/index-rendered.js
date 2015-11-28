5a29be1f21a5bf1a454c755351f2e637c7d7c3fa
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-11-28 22:31:58 +0000

    Drag and drop working

--- client/components/index/scripts/index-rendered.js
+++ client/components/index/scripts/index-rendered.js
@@ -5,7 +5,14 @@
 
 Template.home.rendered = function(){
 
-    var dnd = dragula([document.querySelector('#index-cards-holder')]);
+    var dnd = dragula([document.querySelector('#index-cards-holder')], {
+
+        // Only draggable with the handle
+        moves: function (el, container, handle) {
+            return $(handle).hasClass('handle');
+        }
+
+    });
 
     dnd.on('drag', function(el, source){
 
