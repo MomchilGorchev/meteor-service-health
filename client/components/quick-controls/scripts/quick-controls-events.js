48b497648fe8caa3d7ab5af340277560e0454b8c
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-11-30 11:13:11 +0000

    Client functionality for new order

--- client/components/quick-controls/scripts/quick-controls-events.js
+++ client/components/quick-controls/scripts/quick-controls-events.js
@@ -5,6 +5,9 @@
 
 Template.quickControls.events({
 
+    /**
+     * No save of the new order - hide the element
+     */
     'click #no-save': function(e, t){
         e.preventDefault();
 
@@ -13,12 +16,36 @@ Template.quickControls.events({
         parent.removeClass('enabled');
     },
 
+    /**
+     * Grab and saves all visible items with new order
+     */
     'click #yes-save': function(e, t){
-
         e.preventDefault();
 
-        // TODO - implement dr update
+        // Cache elements
+        var cards = $('.card'),
+            newOrderList = [];
 
+        // Iterate and match the store the itemId with the
+        // corresponding position
+        for(var i = 0, count = cards.length; i < count; i++){
+
+            var current = cards[i];
+
+            // Push to an array to be sent to the server
+            newOrderList.push({
+                itemId: current.getAttribute('data-itemid'),
+                newOrder: count - i
+            });
+        }
+
+        // TODO maybe we can do this functionality on every drag/drop
+
+        // Call the service with the prepared data
+        Meteor.call('updateEndpointsOrder', newOrderList, function(err, res){
+            err ? Materialize.toast('Error: '+ err.message , 3000)
+                : Materialize.toast('Services order saved!' , 3000);
+        });
     }
 
 });
