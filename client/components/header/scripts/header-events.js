d2905c59f7c929233f38afabb03edbe618ffeb69
Momchil Gorchev <momchil.gorchev@gmail.com>
2016-02-08 15:13:17 +0000

    Moving Search functionality to quick controls

--- client/components/header/scripts/header-events.js
+++ client/components/header/scripts/header-events.js
@@ -5,45 +5,5 @@
 Template.header.events({
     'click .logout': function(e, t){
         Meteor.logout();
-    },
-
-    'click .refresh-list': function(e, t) {
-        var trigger = $(e.currentTarget),
-            icon = trigger.find('.material-icons');
-        icon.addClass('rotating');
-        console.log('Services check initiated...');
-        Meteor.call('checkServicesStatus', function (err, res) {
-            err ? Bert.alert('Error: '+ err.message , 'danger')
-                : Bert.alert('Services list successfully reloaded!', 'success');
-
-            icon.removeClass('rotating');
-        });
-    },
-
-    'keyup #search': function(e, t){
-        var filter = $(e.currentTarget).val();
-        var items = $('.card');
-        $.each(items, function(){
-            var current = $(this);
-            if(current.text().search(new RegExp(filter, 'i')) < 0){
-                current.hide();
-            } else {
-                current.show();
-            }
-        });
-    },
-
-    'click #search__toggle': function(e, t){
-        e.preventDefault();
-
-        var trigger = $(e.currentTarget),
-            parent = trigger.closest('li'),
-            input = parent.find('.search__field');
-
-        log(input);
-
-        input.toggleClass('search__field--expanded');
-
-
     }
 });
--- client/components/quick-controls/scripts/quick-controls-events.js
+++ client/components/quick-controls/scripts/quick-controls-events.js
@@ -5,6 +5,46 @@
 
 Template.quickControls.events({
 
+    'click .refresh-list': function(e, t) {
+        var trigger = $(e.currentTarget),
+            icon = trigger.find('.material-icons');
+        icon.addClass('rotating');
+        console.log('Services check initiated...');
+        Meteor.call('checkServicesStatus', function (err, res) {
+            err ? Bert.alert('Error: '+ err.message , 'danger')
+                : Bert.alert('Services list successfully reloaded!', 'success');
+
+            icon.removeClass('rotating');
+        });
+    },
+
+    'keyup #search': function(e, t){
+        var filter = $(e.currentTarget).val();
+        var items = $('.card');
+        $.each(items, function(){
+            var current = $(this);
+            if(current.text().search(new RegExp(filter, 'i')) < 0){
+                current.addClass('card--filtered');
+            } else {
+                current.removeClass('card--filtered');
+            }
+        });
+    },
+
+    'click #search__toggle': function(e, t){
+        e.preventDefault();
+
+        var trigger = $(e.currentTarget),
+            parent = trigger.closest('li'),
+            input = parent.find('.search__field');
+
+        log(input);
+
+        input.toggleClass('search__field--expanded');
+
+
+    },
+
     /**
      * Pagination controls
      * Use real DB data to calculate active (viewed) items
